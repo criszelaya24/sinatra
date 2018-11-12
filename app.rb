f5a41c450dfca9e91f438a082c6d1a5464d7c512
Cristopher Palacios <criszelaya24@hotmail.com>
2018-11-12 16:21:25 +0000

    Adding cute cat with border

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'shotgun'
+
 set :port, 8080
 
 get '/' do
@@ -6,5 +8,13 @@ get '/' do
 end
 
 get '/secret' do
-	'This is a secret area!'
+	'This is a secret area!! Go away!'
 end
+
+get '/cat' do
+	"<div style='border: 3px dashed red'>
+    <img src='http://bit.ly/1eze8aE'>
+   </div>"
+end
+
+# shotgun app.rb -p 4567 // Once is installed sinatra with: gem install shotgun // run the following command and giving the default port
