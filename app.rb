d9ff8ed1110681d3a4848567a498fd3ad7a40a4d
Cristopher Palacios <criszelaya24@hotmail.com>
2018-11-12 16:31:59 +0000

    Moving apart our html and letting that app.rb be our driver

--- app.rb
+++ app.rb
@@ -12,9 +12,7 @@ get '/secret' do
 end
 
 get '/cat' do
-	"<div style='border: 3px dashed red'>
-    <img src='http://bit.ly/1eze8aE'>
-   </div>"
+	erb(:index)
 end
 
 # shotgun app.rb -p 4567 // Once is installed sinatra with: gem install shotgun // run the following command and giving the default port
